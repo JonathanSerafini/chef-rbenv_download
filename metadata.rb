0cfb813daee4a0bcd11ea086a1b40866f89bb30b
Jonathan Serafini <jonathan@serafini.ca>
2016-06-10 12:27:11 -0400

    Kill all typos

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ issues_url 'https://github.com/JonathanSerafini/chef-ruby_rbenv_download/issues'
 license 'apache2'
 description 'Provides ruby_rbenv_download to download ruby from binaries'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-chef_verison '>= 12.7'
+chef_version '>= 12.7'
 version '0.1.0'
 depends 'ruby_rbenv', '~> 1.0.0'
