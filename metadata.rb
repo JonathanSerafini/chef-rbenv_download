ae5cd47d3cebd177babac1ee46e3010db26cd9fa
Jonathan Serafini <jonathan@serafini.ca>
2016-06-10 12:25:43 -0400

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,11 @@
 name 'ruby_rbenv_download'
 maintainer 'Jonathan Serafini'
 maintainer_email 'jonathan@serafini.ca'
+source_url 'https://github.com/JonathanSerafini/chef-ruby_rbenv_download'
+issues_url 'https://github.com/JonathanSerafini/chef-ruby_rbenv_download/issues'
 license 'apache2'
-description 'Installs/Configures ruby_rbenv_download'
-long_description 'Installs/Configures ruby_rbenv_download'
+description 'Provides ruby_rbenv_download to download ruby from binaries'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+chef_verison '>= 12.7'
 version '0.1.0'
 depends 'ruby_rbenv', '~> 1.0.0'
